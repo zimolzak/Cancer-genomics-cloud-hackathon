a6a02ff03d877ca8eeab6828780f89b050d3032d
Andrew Zimolzak <andyzimolzak@gmail.com>
2016-04-02 16:23:56 -0400

    Loop over 5 cancers, 4 genes.

--- stratified_classifier.R
+++ stratified_classifier.R
@@ -1,2 +1,20 @@
+basedir = "/Users/ajz/Desktop/CA Genomics Cloud/expression-cnv-variant/"
+
+cancers = c('brca','coad','luad','lusc','prad')
+genes = c('tp53', 'ttn', 'muc16', 'kras')
+
+basefile = 'cgc_case_explorer_selected_data.csv'
+
+for(ca in cancers)
+{
+	for(ge in genes)
+	{
+		fn = paste(ca, ge, basefile)
+		print(paste(basedir, fn, sep=""))
+		df = read.csv(paste(basedir, fn, sep=""))
+		print(dim(df))
+	}
+}
+
 bt = read.csv("/Users/ajz/Desktop/CA Genomics Cloud/expression-cnv-variant/brca tp53 cgc_case_explorer_selected_data.csv")
 

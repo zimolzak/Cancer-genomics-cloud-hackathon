2aab24a5660e81a628421ac821f8ff70a11a862d
Andrew Zimolzak <andyzimolzak@gmail.com>
2016-04-03 09:19:57 -0400

    Better way of avoiding strata with only 1 class.

--- stratified_classifier.R
+++ stratified_classifier.R
@@ -6,6 +6,7 @@ cancers = c('brca','coad','luad','lusc','prad')
 genes = c('tp53', 'ttn', 'muc16', 'kras')
 basefile = 'cgc_case_explorer_selected_data.csv'
 grsq_vals = data.frame()
+make_many_plots = FALSE
 
 for(ca in cancers)
 {
@@ -13,20 +14,23 @@ for(ca in cancers)
 	{
 		filename = paste(ca, ge, basefile)
 		pathname = paste(basedir, filename, sep="")
-		if((ca=='prad' & ge=='kras') | (ca=='brca' & ge=='kras') | (ca=='lusc' & ge=='kras')){
+		if(ca=='prad' & ge=='kras'){
+			# do not attempt to open. I have prior knowledge this file doesn't exist. Less adhoc later.
 			next
 		}
 		df = read.csv(pathname)
-
+		if(dim(table(df$Variant.Classification)) == 1){
+			# it has only one class so skip trying to model this stratum
+			next
+		}
 		Xb = data.frame(expression=df$Expression, cnv=df$CNV, isMissense=(df$Variant.Classification=='Missense_Mutation'))
-		qplot(data=Xb, x=expression, y=cnv, color=isMissense)
 		model=earth(isMissense ~ ., data=Xb)
-		plotmo(model)
-		plot(evimp(model))
-		plot(model)
+		if(make_many_plots){
+			plotmo(model)
+			plot(evimp(model))
+			plot(model)
+			qplot(data=Xb, x=expression, y=cnv, color=isMissense)
+		}
 		print(paste(ca, ge, model$grsq)) # 0.66
 	}
 }
-
-bt = read.csv("/Users/ajz/Desktop/CA Genomics Cloud/expression-cnv-variant/brca tp53 cgc_case_explorer_selected_data.csv")
-

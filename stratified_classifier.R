e4b718195c2c71be99f8eeb93d4be5c29d795d13
Andrew Zimolzak <andyzimolzak@gmail.com>
2016-04-03 08:54:55 -0400

    Now binarized only

--- stratified_classifier.R
+++ stratified_classifier.R
@@ -20,14 +20,6 @@ for(ca in cancers)
 
 bt = read.csv("/Users/ajz/Desktop/CA Genomics Cloud/expression-cnv-variant/brca tp53 cgc_case_explorer_selected_data.csv")
 
-X = data.frame(expression=bt$Expression, cnv=bt$CNV, variant_class=bt$Variant.Classification)
-qplot(data=X, x=expression, y=cnv, color=variant_class)
-model=earth(variant_class ~ ., data=X)
-plotmo(model, nresponse=4)
-plot(evimp(model))
-plot(model, nresponse=4)
-model$grsq # 0.35
-
 Xb = data.frame(expression=bt$Expression, cnv=bt$CNV, isMissense=(bt$Variant.Classification=='Missense_Mutation'))
 qplot(data=Xb, x=expression, y=cnv, color=isMissense)
 model=earth(isMissense ~ ., data=Xb)
